5599139ae3398ff2565e236e816b853a9474610c
cocofox1902 <colas.renard1902@gmail.com>
2021-10-01 11:32:11 +0000

    0

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Main
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char n = 48;
+    char x = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+    while (x <= 102)
+    {
+        putchar(x);
+        x++;
+    }
+	putchar ('\n');
+	return (0);
+}
