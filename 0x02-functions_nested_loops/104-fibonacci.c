93ff9bae500cbfeba1fcf88ae47b4e10d3c97514
cocofox1902 <colas.renard1902@gmail.com>
2021-10-05 21:44:38 +0000

    0

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,42 +9,43 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	unsigned long a = 0, b = 1, sum;
+	unsigned long m1a, m1b, m2a, m2b;
+	unsigned long ma, mb;
 
 	for (count = 0; count < 92; count++)
 	{
-		sum = fib1 + fib2;
+		sum = a + b;
 		printf("%lu, ", sum);
 
-		fib1 = fib2;
-		fib2 = sum;
+		a = b;
+		b = sum;
 	}
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+	m1a = a / 10000000000;
+	m2a = b / 10000000000;
+	m1b = a % 10000000000;
+	m2b = b % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		ma = m1a + m2a;
+		mb = m1b + m2b;
+		if (m1b + m2b > 9999999999)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			ma += 1;
+			mb %= 10000000000;
 		}
 
-		printf("%lu%lu", half1, half2);
+		printf("%lu%lu", ma, mb);
 		if (count != 98)
+		{
 			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		}
+		m1a = m2a;
+		m1b = m2b;
+		m2a = ma;
+		m2b = mb;
 	}
 	printf("\n");
 	return (0);
