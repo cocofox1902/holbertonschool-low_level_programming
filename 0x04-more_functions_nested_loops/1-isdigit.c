3f2f3a0cc25608d5e256372a0666f3461e27be55
cocofox1902 <colas.renard1902@gmail.com>
2021-10-07 08:25:36 +0000

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - digit
+ * @c: third integer
+ * Return: upper
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c >= '9')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
