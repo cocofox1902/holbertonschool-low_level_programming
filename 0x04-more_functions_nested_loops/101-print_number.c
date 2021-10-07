ecfb68c131e3b8642a4dc9aea8cf5cb8804bc51b
cocofox1902 <colas.renard1902@gmail.com>
2021-10-07 14:59:25 +0000

    0

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: int
+ * Return: multipe
+ */
+
+void print_number(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar(45);
+	}
+	else
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+
+	_putchar(a % 10 + 48);
+}
