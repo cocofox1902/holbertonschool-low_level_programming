0fad3d40b0613317d7494c9651ed80c5a16e5b85
cocofox1902 <colas.renard1902@gmail.com>
2021-10-07 08:19:34 +0000

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - upper
+ * @c: third integer
+ * Return: upper
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c >= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
