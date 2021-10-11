203b5f9b89ecfffe248047702fd95ac020148041
cocofox1902 <colas.renard1902@gmail.com>
2021-10-11 12:56:42 +0000

    0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: char
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int max, divmax;
+	for (max = 0; str[max] != '\0'; max++)
+	{
+		continue;
+	}
+	divmax = max / 2;
+	if (max % 2 != 0)
+	{
+		max = (max - 1) / 2;
+	}
+	while (str[divmax] != '\0')
+	{
+		_putchar(str[divmax]);
+		divmax++;
+	}
+	_putchar('\n');
+}
