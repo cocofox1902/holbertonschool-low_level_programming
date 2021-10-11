62062abb991541f37a18682720cde7ac1e315158
cocofox1902 <colas.renard1902@gmail.com>
2021-10-11 09:57:35 +0000

    0

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,34 +4,32 @@ int _strlen(char *s);
 void print_rev(char *s);
 
 /**
- * print_rev - print a string in reverse
- * @s: char
- * Return: Always 0.
+ * print_rev - reverse print
+ * @s: int
+ * Return: Always 0
  */
-
 void print_rev(char *s)
 {
 	int len;
 
-	for (len = _strlen(s) - 1; len >= 0; len++)
+	for (len = _strlen(s) - 1; len >= 0; len--)
 	{
 		_putchar(s[len]);
 	}
 	_putchar('\n');
 }
-/**
- * _strlen - lenght string
- * @s: int
- * Return: Always 0.
- */
 
+/**
+ * _strlen - stringh lengh
+ * @s: int
+ * Return: Always 0
+ */
 int _strlen(char *s)
 {
-
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	return (0);
+	return (i);
 }
