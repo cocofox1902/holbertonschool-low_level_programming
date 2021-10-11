ff7f89943d4f8510748c391cc029e112b4bf854b
cocofox1902 <colas.renard1902@gmail.com>
2021-10-11 00:00:40 +0000

    0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - points swap
+ * @a: int
+ * @b: int
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	*a = n;
+	*b = n;
+	*a = *b;
+}
