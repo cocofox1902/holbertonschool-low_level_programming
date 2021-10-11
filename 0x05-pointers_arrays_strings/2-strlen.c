0772f9595bc8f7b3103275cceb88196d47031e8e
cocofox1902 <colas.renard1902@gmail.com>
2021-10-11 00:15:05 +0000

    0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - points swap
+ * @s: int
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
