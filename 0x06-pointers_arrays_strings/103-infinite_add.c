6e4645506bc805f48af3097171d85d30ab30981f
cocofox1902 <colas.renard1902@gmail.com>
2021-10-14 13:31:03 +0000

    code update

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * infinite_add - Main
+ * @n1: int
+ * @n2: int
+ * @r: int
+ * @size_r: int
+ * Return: return
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
