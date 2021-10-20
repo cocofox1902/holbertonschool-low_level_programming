8251ed958182af6568ecc9937dcda5d73e433211
cocofox1902 <colas.renard1902@gmail.com>
2021-10-20 22:14:08 +0000

    tool

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,9 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_(int i, int j);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int length(char *s);
+int palindrome(char *s, int l, int a);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
