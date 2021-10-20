6469a07adc6f7e472a178e2db6fbcece8e2f6f07
cocofox1902 <colas.renard1902@gmail.com>
2021-10-20 09:34:40 +0000

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Write a function that prints string followed by new line.
+ * @s: char
+ * Return: None
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
