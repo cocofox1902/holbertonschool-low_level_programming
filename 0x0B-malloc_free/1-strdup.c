2121c6ba26e9a5ee5e0ceb34e60fa6936193f763
cocofox1902 <colas.renard1902@gmail.com>
2021-10-26 09:51:45 +0000

    return a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Write a function that returns a pointer
+ * to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+ * @str: str
+ * Return: v
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int s;
+	char *v;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (s = 0; str[s]; s++)
+	{
+		continue;
+	}
+
+	v = (char *)malloc(s * sizeof(char) + 1);
+
+	if (v == NULL)
+	{
+		return (NULL);
+	}
+
+	for (s = 0; str[s]; s++)
+	{
+		v[s] = str[s];
+	}
+
+	return (v);
+}
