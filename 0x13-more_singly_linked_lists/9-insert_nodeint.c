8c4c434dbcaa151804091b7e73a37b9fdc6ae960
cocofox1902 <colas.renard1902@gmail.com>
2021-11-16 12:38:51 +0000

    add node in a list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_noint_at_index - insert a end no
+ * @head: list
+ * @idx: unsigned int
+ * @n: int
+ * Return: list
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *end, *tmp = *head;
+	unsigned int no = 0;
+
+	end = malloc(sizeof(listint_t));
+
+	if (end == NULL)
+		return (NULL);
+
+	end->n = n;
+
+	if (idx == 0)
+	{
+		end->next = tmp;
+		*head = end;
+		return (end);
+	}
+
+	while (no < (idx - 1))
+	{
+		if (tmp == NULL || tmp->next == NULL)
+			return (NULL);
+
+		tmp = tmp->next;
+		no++;
+	}
+
+	end->next = tmp->next;
+	tmp->next = end;
+
+	return (end);
+}
