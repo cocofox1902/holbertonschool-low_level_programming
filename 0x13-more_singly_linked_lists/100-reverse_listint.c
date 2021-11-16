f131d777bbf5c123c56992b8b111fe5498de8ea8
cocofox1902 <colas.renard1902@gmail.com>
2021-11-16 13:36:45 +0000

    reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - revesre a list
+ * @head: list
+ * Return: lsit reversed
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp, *end = *head;
+	unsigned int n = 0;
+
+	if (end == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(end);
+		return (1);
+	}
+
+	while (n < (index - 1))
+	{
+		if (end->next == NULL)
+			return (-1);
+
+		end = end->next;
+		n++;
+	}
+
+	temp = end->next;
+	end->next = temp->next;
+
+	free(temp);
+	return (1);
+}
