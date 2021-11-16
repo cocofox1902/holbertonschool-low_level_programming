b4783ce7724628df7f4b412119d06b1dc86c2384
cocofox1902 <colas.renard1902@gmail.com>
2021-11-16 09:33:32 +0000

    error execution

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,11 +13,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = (struct listint_s *)malloc(sizeof(struct listint_s));
-	new_node->n = n;
-	new_node->next = NULL;
 
 	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 		*head = new_node;
