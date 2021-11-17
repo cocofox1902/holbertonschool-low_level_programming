0e658382b457d5ec073ce78510eb56de4befb9cf
cocofox1902 <colas.renard1902@gmail.com>
2021-11-17 09:45:14 +0000

    library

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,5 +34,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int test_same(const listint_t *head, const listint_t *tested, size_t i);
 
 #endif /* LISTS_H */
