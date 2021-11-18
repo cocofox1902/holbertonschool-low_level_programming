365ed73e4bd602522d73eaa01e3a245013d9a701
cocofox1902 <colas.renard1902@gmail.com>
2021-11-18 13:32:53 +0000

    print int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert into int
+ * @b: coonstant
+ * Return: int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bin = 0, times = 1;
+	int lenght;
+
+	for (lenght = 0; b[lenght];)
+		lenght++;
+
+	for (lenght -= 1; lenght >= 0; lenght--)
+	{
+		if (b[lenght] != '0' && b[lenght] != '1')
+			return (0);
+		bin += (b[lenght] - '0') * times;
+		times *= 2;
+	}
+	return (bin);
+}
