f59a0f9c7c2af40ce884b41cdcb371b8fc9eeb4a
cocofox1902 <colas.renard1902@gmail.com>
2021-11-18 13:35:39 +0000

    library

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif /*BIT_MANIPULATION*/
