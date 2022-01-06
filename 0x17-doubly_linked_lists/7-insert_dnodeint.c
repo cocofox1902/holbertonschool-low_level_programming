c1aa93411a3dfd74edc9e005c1fae595972e89ee
cocofox1902 <colas.renard1902@gmail.com>
2022-01-06 14:32:58 +0000

    add task done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,16 +25,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 
-		new_node = malloc(sizeof(dlistint_t));
-
 	if (head->next == NULL)
 		return (add_dnodeint_end(h, n));
 
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->next = head;
-	new_node->prev = head->prev;
-	head->prev->next = new_node;
-	head->prev = new_node;
+	new_node->next = head->next;
+	head->next->prev = new_node;
+	head->next = new_node;
 
 	return (new_node);
 }
