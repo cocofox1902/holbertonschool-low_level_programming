803e56f0fb48bdf2f84082f5725bebf3370f85b1
cocofox1902 <colas.renard1902@gmail.com>
2022-01-06 15:23:33 +0000

    add task done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = NULL, *head = *h;
+	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
@@ -19,8 +20,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; idx != 1 && head->next != NULL; idx--)
+	while (i < idx && browse->next != NULL)
 	{
+		i++;
 		head = head->next;
 	}
 
