67ed7432267552eed772031594a7e8b42927b2c6
cocofox1902 <colas.renard1902@gmail.com>
2022-01-06 14:34:31 +0000

    add task done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = head;
+	new_node->prev = head;
 	new_node->next = head->next;
 	head->next->prev = new_node;
 	head->next = new_node;
