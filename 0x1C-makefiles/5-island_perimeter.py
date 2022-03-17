c9b8f0216bb725777a2e18c82d92f7a7659b5670
cocofox1902 <colas.renard1902@gmail.com>
2022-03-17 11:56:22 +0000

    add all

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 island perimeter
-'''
+"""
 
 
 def island_perimeter(grid):
@@ -10,8 +10,8 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 if i - 1 >= 0 and grid[i - 1][j] == 1:
-                        result = result - 2
+                    result = result - 2
                 if j - 1 >= 0 and grid[i][j - 1] == 1:
-                        result = result - 2
+                    result = result - 2
                 result = result + 4
     return(result)
